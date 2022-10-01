08b5f12e40ad9ecee64806fff277b4b2cac0eb88
Nurizki <nutrisi1udin@gmail.com>
2022-10-01 09:25:19 +0700

    tugas 1 php
    tugas niomic

---
+++ tugas1_1.php
@@ -0,0 +1,25 @@
+<?php
+$ayamGoreng = 13000;
+$ayamBakar = 15000;
+$ayamSayur = 13000;
+$tempeGoreng = 1000;
+$tahuGoreng = 1000;
+$soto = 3000;
+$nasiPutih = 5000;
+$esTeh = 4000;
+$esTebu = 5000;
+
+echo "<h1>Daftar Menu</h1>";
+echo "<ul>";
+echo "<li>Ayam Goreng = Rp. $ayamGoreng</li>";
+echo "<li>Ayam Bakar = Rp. $ayamBakar</li>";
+echo "<li>Ayam Sayur = Rp. $ayamSayur</li>";
+echo "<li>Tempe Goreng = Rp. $tempeGoreng</li>";
+echo "<li>Tahu Goreng = Rp. $tahuGoreng</li>";
+echo "<li>Soto = Rp. $soto</li>";
+echo "<li>Nasi Putih = Rp. $nasiPutih</li>";
+echo "<li>Es Teh = Rp. $esTeh</li>";
+echo "<li>Es Tebu = Rp. $esTebu</li>";
+echo "</ul>";
+
+?>
---
+++ tugas1_2.php
@@ -0,0 +1,37 @@
+<?php
+$cireng = 1000; $esDuren=15000; $esMilo=20000; $esTeh=5000; $piscok=1000;
+
+echo "<h1>Daftar Menu</h1>";
+echo "<table border='1'>";
+echo "<tr>";
+echo "<th>No</th>";
+echo "<th>Nama Menu</th>";
+echo "<th>Harga</th>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>1</td>";
+echo "<td>Cireng</td>";
+echo "<td>Rp. $cireng</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>2</td>";
+echo "<td>Es Duren</td>";
+echo "<td>Rp. $esDuren</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>3</td>";
+echo "<td>Es Milo</td>";
+echo "<td>Rp. $esMilo</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>4</td>";
+echo "<td>Es Teh</td>";
+echo "<td>Rp. $esTeh</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>5</td>";
+echo "<td>Piscok</td>";
+echo "<td>Rp. $piscok</td>";
+echo "</tr>";
+echo "</table>";
+?>
